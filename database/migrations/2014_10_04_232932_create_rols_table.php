b54d66c8a73ade3408261887b21e383a694ad07c
Jackson Gonzalez <34948692+JacksonGonzalez@users.noreply.github.com>
2020-11-04 19:29:13 -0500

    se crean las migraciones de rols y users

---
+++ database/migrations/2014_10_04_232932_create_rols_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rols', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->boolean('productos')->default(0);
+            $table->boolean('autorizar')->default(0);
+            $table->boolean('categorias')->default(0);
+            $table->boolean('banner')->default(0);
+            $table->boolean('usuarios')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rols');
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
+            $table->string('usuario');
+            $table->foreignId('idRol');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->bigInteger('telefono')->nullable();
+            $table->string('pais');
+            $table->string('departamento');
+            $table->string('ciudad');
+            $table->string('direccion')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
+
+
+            $table->foreign('idRol')->references('id')->on('rols')->onDelete('cascade');;
         });
     }
 
