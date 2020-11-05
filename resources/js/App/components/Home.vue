5170cbb3212c06cecb0bf3e1b648c8a002529ecf
Jackson Gonzalez <34948692+JacksonGonzalez@users.noreply.github.com>
2020-11-05 14:37:37 -0500

    se configura vue, vuex y vue-router en el proyecto

---
+++ resources/js/App/components/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+        
+          <h4>SALES SYSTEM</h4>
+          <h5>Sistema de Gestion para Venta y Compra de productos</h5>
+          <p>Una plataforma creada para gestionar las compras y ventas de cualquier producto.</p>
+          <!-- <routerLink to="/login"><button class="btn btn-success btn-lg">Iniciar Sesion</button></routerLink> -->
+    
+    
+  </div>
+</template>
+
+<script>
+    // import Dashboard from './Dashboard';
+
+    export default {
+        name: 'Home',
+        data(){
+            return {
+                loading: false
+            }
+        },
+        components:{
+            // 'dashboard' : Dashboard
+        },
+        mounted() {
+            
+        }
+    }
+</script>
+
+<style scoped>
+      .carousel-caption {
+        bottom: 40%;
+      }
+      h4{
+        font-size: 450% ;
+      }
+      h5{
+        font-size: 200% ;
+      }
+      img{
+        /* opacity: 0.4; */
+        background-color: rgba(0,0,0,0.8);
+        filter:brightness(0.4);
+        max-height: 100%;
+        
+      }
+      p{
+        font-size: 120%;
+      }
+      #fondo{
+        min-height:100%;
+        max-height: 100%;
+        background-size:cover;
+        background-repeat: no-repeat;
+      }
+      router-link{
+          text-decoration: none;
+      }
+      router-link a{
+          text-decoration: none;
+      }
+      router-link a{
+          text-decoration: none !important;
+      }
+</style>
---
+++ resources/js/App/routes.js
@@ -0,0 +1,98 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from './components/Home';
+// import Dashboard from './components/Dashboard';
+// import Login from './components/Login';
+// import MenuMain from './components/MenuMain';
+// import Category from './components/Category';
+// import Client from './components/Client';
+// import Income from './components/Income';
+// import IncomeReport from './components/IncomeReport.vue';
+// import Order from './components/Order.vue';
+// import Product from './components/Product.vue';
+// import Rol from './components/Rol.vue';
+// import Sale from './components/Sale.vue';
+// import SaleReport from './components/SaleReport.vue';
+// import Supplier from './components/Supplier.vue';
+// import User from './components/User.vue';
+
+Vue.use(VueRouter);
+
+export const routes = [
+    {
+        path : '/',
+        component: Home,
+
+    },
+    // {
+    //     path : '/login',
+    //     component: Login,
+
+    // },
+    // {
+    //     path : '/dashboard',
+    //     component: Dashboard,
+
+    // },
+    // {
+    //     path : '/menu-main',
+    //     component: MenuMain,
+
+    // },
+    // {
+    //     path : '/categorias',
+    //     component: Category,
+
+    // },
+    // {
+    //     path : '/productos',
+    //     component: Product,
+
+    // },
+    // {
+    //     path : '/ingresos',
+    //     component: Income,
+
+    // },
+    // {
+    //     path : '/proveedor',
+    //     component: Supplier,
+
+    // },
+    // {
+    //     path : '/pedidos',
+    //     component: Order,
+
+    // },
+    // {
+    //     path : '/ventas',
+    //     component: Sale,
+
+    // },
+    // {
+    //     path : '/clientes',
+    //     component: Client,
+
+    // },
+    // {
+    //     path : '/usuarios',
+    //     component: User,
+
+    // },
+    // {
+    //     path : '/roles',
+    //     component: Rol,
+
+    // },
+    // {
+    //     path : '/reporte-ingreso',
+    //     component: IncomeReport,
+
+    // },
+    // {
+    //     path : '/reporte-ventas',
+    //     component: SaleReport,
+
+    // },
+
+];
